036122aec1235e3ce29692858144e9cc51ba59e0
amandeepsinghbhador <145202142+amandeepsinghbhador@users.noreply.github.com>
2024-10-29 10:00:24 +0530

    Add files via upload

---
+++ DAY27PRB01.py
@@ -0,0 +1,32 @@
+import math
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+T = int(input())
+results = []
+
+
+for _ in range(T):
+    N = int(input())
+    if is_prime(N):
+        results.append("yes")
+    else:
+        results.append("no")
+
+
+for result in results:
+    print(result)
